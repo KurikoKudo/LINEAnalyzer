f7a05ab6e893c812a59f208dc838adf46ae4b951
Kuriko <e155407w@st.u-gakugei.ac.jp>
2018-04-22 02:57:42 +0900

    modify display information

--- cmd/go-lineanalyzer/main.go
+++ cmd/go-lineanalyzer/main.go
@@ -4,6 +4,7 @@ import (
 	"os"
 	"github.com/KurikoKudo/go-lineanalyzer"
 	"fmt"
+	"time"
 )
 
 func main() {
@@ -22,5 +23,19 @@ func main() {
 	fmt.Println("保存日時：",lineAllTalk.SaveDate)
 	fmt.Println("トーク総数：",len(lineAllTalk.Massages))
 
+	developMassages := 0
+	designMassages := 0
+
+	for _,v := range lineAllTalk.Massages {
+		if !time.Date(2017, time.December, 11, 0, 0, 0, 0, time.UTC).After(v.SendTime) {
+			developMassages++
+		} else {
+			designMassages++
+		}
+	}
+
+	fmt.Println("------------トーク数内訳------------")
+	fmt.Println("設計ターム（2017/12/10以前）：",designMassages)
+	fmt.Println("実装ターム（2017/12/11以降）：",developMassages)
 
 }
